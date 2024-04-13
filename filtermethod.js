add65cd1321dd7f3fb300b603089b7f2ccd29936
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-04-14 00:39:50 +0530

    find the repeated element and find the array without repeated element

--- filtermethod.js
+++ filtermethod.js
@@ -1,5 +1,20 @@
+// const numbers = [1,2,3,4,6,5,6,7,8,9];
+// const result = numbers.filter((curElem)=>{
+//    return curElem > 4;
+// });
+// console.log(result);
+
+// let value = 6;
+// const numbers = [1,2,3,4,6,5,6,7,8,9];
+// let updateCart = numbers.filter((currentElement)=>{
+//     return currentElement === value;
+// });
+// console.log(updateCart);
+
+let value = 6;
 const numbers = [1,2,3,4,6,5,6,7,8,9];
-const result = numbers.filter((curElem)=>{
-   return curElem > 4;
+let updateCart = numbers.filter((currentElement)=>{
+    // return currentElement === value;
+       return currentElement !== value;
 });
-console.log(result);
+console.log(updateCart);
