9e0536f527a46c4a11c7ae4f772ecc129b03ac7a
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-04-14 23:59:35 +0530

    string with single double quotes and also with backslash

---
+++ Strings.js
@@ -0,0 +1,6 @@
+const singleString = "Ritik  \"chaurasia\"  Rahul chaurasia ";
+const singleString1 = 'Ritik  "chaurasia"  Rahul chaurasia ';
+const singleString2 = "Ritik  \\chaurasia Rahul chaurasia ";
+console.log(singleString);
+console.log(singleString1);
+console.log(singleString2);
