676bb3b40bfa4ec1e723a6cc7648e2098c34dd42
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-04-17 19:26:47 +0530

    Create a new button element. Give it a text "click me " , background color of red and text color of white.Insert the button as the first element inside the body tag

--- index4.js
+++ index4.js
@@ -1,9 +1,9 @@
-// Create a new button element. Give it a text "click me " , background color of red and text color of white.Insert the button as the first element inside the body tag
+//Q1. Create a new button element. Give it a text "click me " , background color of red and text color of white.Insert the button as the first element inside the body tag
 
 
 let newBtn = document.createElement("button");
 
-newBtn.innerText="click me";
+newBtn.innerText="click me !";
 console.log(newBtn);
  newBtn.style.color="white";
  newBtn.style.backgroundColor="red";
@@ -11,3 +11,7 @@ console.log(newBtn);
  document.querySelector("body").prepend(newBtn);
 // let s = document.querySelector("body");
 // s.prepend(newBtn);
+
+// let para = document.querySelector("p");
+// para.setAttribute("class","nn");
+// para.classList.add("newclass");
