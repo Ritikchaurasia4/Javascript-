98af7332cc9a436390174d600adf69ea24c7c2fd
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-04-19 22:43:12 +0530

    Method Overriding inheritane

--- classes.js
+++ classes.js
@@ -4,20 +4,51 @@
 
 // class is the bluprint for the creation of object.
 
-class ToyotaCar {
-    start() {
-        console.log("start");
-    }
-    stop(){
-        console.log("stop");
-    }
-    setBrand(brand) {
-        this.brandName = brand;
-    }
-}
-// create object from class
+//------ When we create an object from the classes by the syntax ' let fortuner = new ToyotaCar() ' then the new keyword find a constructor in th classes but if there is no any one constructor in classes then the javascript automaticaly created a constructor .
+//  ------ Show below
 
-let fortuner = new ToyotaCar();
-fortuner.setBrand("fortuner");
-let swift = new ToyotaCar();
-swift.setBrand("Swift");
+    // class ToyotaCar {
+    //     start() {
+    //         console.log("start");
+    //     }
+    //     stop(){
+    //         console.log("stop");
+    //     }
+    //     setBrand(brand) {
+    //         this.brandName = brand;
+    //     }
+    // }
+    // // create object from class
+
+    // let fortuner = new ToyotaCar();
+    // fortuner.setBrand("fortuner");
+    // let swift = new ToyotaCar();
+    // swift.setBrand("Swift");
+
+    class ToyotaCar {
+  
+        // we can paas argument also in constructor
+
+        constructor(brand,mileage) {
+            console.log("creating new object");
+            this.brand = brand;
+            this.mileage = mileage;
+        }
+
+        start() {
+            console.log("start");
+        }
+        stop(){
+            console.log("stop");
+        }
+        // setBrand(brand) {
+        //     this.brandName = brand;
+        // }
+    }
+    // create object from class
+    
+    let fortuner = new ToyotaCar("fortuner",10); // constructor invoke 
+    console.log(fortuner);
+    let swift = new ToyotaCar("swift",12);  // constructor invoke 
+    console.log(swift);
+  
---
+++ inheritance.js
@@ -0,0 +1,48 @@
+// --------------- Inheritance -----------------
+
+// ------- If child and parent have same method , child's method will be used.  --------
+// This method is known as --- Method Overriding ---
+
+    // class parent{
+    //     hello() {
+    //     console.log("Hello");
+    //     }
+    // }
+
+    // class child extends parent {}
+
+    // let obj = new child();
+
+    class person {                // class 
+       
+        constructor() {     // constructor 
+            this.species = "Homo sapiens";
+        }
+
+        eat() {
+           console.log("eat");
+        }
+        sleep() {
+        console.log("sleep");
+        }
+
+        work() {
+            console.log("do nothing");
+        }
+    }
+
+    class Engineer extends person {        // inherit base class
+        work() {
+            console.log("Solve problems");
+        }
+    }
+    class doctor extends person {
+       
+        work() {
+            console.log("Treat person");
+        }
+    }
+
+    let ritikObj = new Engineer();
+
+    
