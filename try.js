6e43420993ea199fa5fc107356ab06ea288f227a
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-05-03 20:55:13 +0530

    Error handling

---
+++ try.js
@@ -0,0 +1,15 @@
+//  Error handling by try catch blocks
+
+let a = 5;
+let b = 6;
+console.log("sum is :",a+b);
+
+try{
+    console.log("sum is :",a+c);
+}catch(error){
+    console.log(error);
+}
+
+console.log("sum is :",a);
+console.log("sum is :",b);
+console.log("sum is :",a-b);
