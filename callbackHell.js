f78986f31168e74174f53aa187fbcb1fb18646cd
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-05-07 00:36:13 +0530

    callback hell

---
+++ callbackHell.js
@@ -0,0 +1,25 @@
+// function getData(dataId){
+//     setTimeout(() => {
+//         console.log("data",dataId)
+//     },2000);
+// }
+// getData(1);
+// getData(2);     // it is executing at a time , but if we want to execute at different time then we use callback hell
+
+function getData(dataId , getnextData){
+    setTimeout(() => {
+        console.log("data",dataId);
+        if(getnextData){
+        getnextData();
+    }
+    },2000);
+}
+getData(1,() => {
+    getData(2,() => {
+        getData(3,() => {
+            getData(4,()=> {
+                getData(5)
+            });
+        });
+    });
+});
